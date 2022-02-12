f8758629dba40d7c5233256b057ab0ee6f8006c1
Anis4whitehat <82126047+Anis4whitehat@users.noreply.github.com>
2022-02-11 19:38:35 -0500

    Add files via upload

--- Clock.js
+++ Clock.js
@@ -1,5 +1,7 @@
 img = "";
 status = "";
+objects = [];
+objectDetector = "";
 
 function preload()
 {
@@ -21,13 +23,14 @@ function modelLoaded()
     objectDetector.detect(img, gotResult);
 }
 
-function gotResult()
+function gotResult(error, results)
 {
     if(error)
     {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
 
 function draw()
@@ -35,5 +38,15 @@ function draw()
     if(status !=undefined)
     {
         image(img, 0, 0, 640, 420);
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
     }
 }
